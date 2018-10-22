6e338ab3cbc44e492cca489d5da6cc16e5806033
Evgeni Pandurski <epandurski@gmail.com>
2018-10-22 21:33:11 +0300

    Add some experiments

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
-import numpy
+import matplotlib.pyplot as plt
+import numpy as np
 
+a1 = np.arange(5)
+a1.append(666)
+
+# t = np.arange(0.0, 2.0, 0.01)
+# s = 1 + np.sin(2 * np.pi * t)
+# plt.plot(t, s)
+
+# plt.xlabel('time (s)')
+# plt.ylabel('voltage (mV)')
+# plt.title('About as simple as it gets, folks')
+# plt.grid(True)
+# plt.savefig("test.png")
+# plt.show()
